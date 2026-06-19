348fcad8444b8d73fc3cc99fe80d1dad4dec42b5
Luis Alberto Osis Leon <luisalbertoosisleon@Luiss-MacBook-Pro.local>
2026-06-18 23:13:18 -0500

    chore: commit init

---
+++ src/main/java/com/reactive_customer_service/ReactiveCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.reactive_customer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveCustomerServiceApplication.class, args);
+	}
+
+}
